438f484826be27f179250b7b7ffa7858d59fc617
BUSHUQING <bsq@bupt.edu.cn>
2017-05-14 19:14:23 +0800

    Create helloworld.java
    这是helloworld

---
+++ helloworld.java
@@ -0,0 +1,10 @@
+public class helloworld {
+	public static void print(){
+		System.out.println("hello world!");
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		helloworld.print();
+	}
+
+}
